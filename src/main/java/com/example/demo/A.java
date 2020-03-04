ebe61c72cb9033afa66d1be967d8ac5e89648bb9
itferrari <itferrari8080>
2020-03-04 21:25:18 +0800

    sd

--- src/main/java/com/example/demo/A.java
+++ src/main/java/com/example/demo/A.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//v1.0tom
+		//v1.0 tom
 		
 		
 		SpringApplication.run(DemoApplication.class, args);
