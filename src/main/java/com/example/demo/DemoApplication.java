37c52478b1f0544837566d0e3705b4799d1adc35
itferrari <itferrari8080>
2020-03-05 11:57:49 +0800

    s

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		//v1.0s
+=======
+		//v1.0 
+>>>>>>> Stashed changes
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
